02aa70b039744bd9259ad237dbc40aada174847b
ricRobo4 <artistr3@proton.me>
2022-10-17 15:55:27 -0700

    Added play again input, and more notes

--- Program.cs
+++ Program.cs
@@ -12,19 +12,47 @@ while(playAgain == true)
 {
     // These will print text to the console to address the player
     Console.WriteLine("\"Oh, hello there! What is your name?\"");
-    Console.Write("Type your name, or type \"END\" to exit: ");
+    Console.Write("Type your name, or type \"END\" to end it: ");
 
     // This will take the user's input, and save it as a string. The line beneath that one will take the user's input and set it to be uppercase
     string userInput = Console.ReadLine();
     userInput = userInput.ToUpper();
     if (userInput == "END")
     {
+        // This will run if the player types "END"
         Console.WriteLine("Alright, goodbye.");
+        // playAgain is now set to false, ending the game
         playAgain = false;
     }
     else
     {
+        // This will run if the player types anything aside from "NO"
         Console.WriteLine($"\"Good evening, {userInput}. I hope you're having a good day today!\"");
-        Console.WriteLine("");
+        // The player will be asked if they'd like to play again, and their input will be recieved and set to uppercase
+        Console.Write("Want to play again? YES or NO: ");
+        string yesNo = Console.ReadLine();
+        yesNo = yesNo.ToUpper();
+
+            if(yesNo == "NO")
+        {
+            // If the player types NO, playAgain is false
+            Console.WriteLine($"Goodbye, {userInput}...");
+            playAgain = false;
+        }
+        else if(yesNo == "YES")
+        {
+            // If the player types YES, playAgain is still true, and the loop continues
+            Console.WriteLine("Alright!");
+        }
+        else
+        {
+            Console.WriteLine("Well, I didn't read a NO...");
+            Console.WriteLine(" ");
+        }
+        
+        
+        
     }
 }
+    
+    
