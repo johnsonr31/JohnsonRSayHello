018dc484544abda34f5772ef4c29acbe938d137e
ricRobo4 <artistr3@proton.me>
2022-10-17 14:53:37 -0700

    Added some code to make the user input uppercase

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Richard Johnson
+// 10-17-2022
+// Say Hello
+//
+
+bool playAgain = true;
+
+while(playAgain == true)
+{
+    Console.WriteLine("\"Oh, hello there! What is your name?\"");
+    Console.Write("Type your name, or type \"END\" to exit:");
+    string userInput = Console.ReadLine();
+    userInput = userInput.ToUpper();
+}
